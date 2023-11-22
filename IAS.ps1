217c08336e8f90536c2b3060631c239c0d341442
fgr178707 <fgr178707@163.com>
2023-11-22 11:12:00 +0800

    1.0

--- IAS.ps1
+++ IAS.ps1
@@ -1,7 +1,7 @@
 # Enable TLSv1.2 for compatibility with older clients
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://raw.githubusercontent.com/fgr178707/IDM/main/IAS_0.8.cmd'
+$DownloadURL = 'https://raw.githubusercontent.com/fgr178707/IDM/main/IAS_1.0.cmd'
 
 $FilePath = "$env:TEMP\IAS.cmd"
 
